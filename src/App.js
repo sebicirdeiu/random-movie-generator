2474f542975adefe2363877586bdf4e032292e56
Sebi Cirdeiu <sebbcirdeiu@yahoo.com>
2023-09-24 14:41:41 +0300

    Create fetch logic to get a list of most popular movies

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
 import "./App.css";
-//import React from "react";
-//import MovieCard from './MovieCard'
+import MovieCard from "./MovieCard";
+import React from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [movies, setMovies] = React.useState("");
+
+  //data to acess the movie API
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OGZhOGFhZjgwMTJhZGJlMmVmZDM3NWExY2U1YjEzMyIsInN1YiI6IjY1MDg1OGQ4M2NkMTJjMDBjYTU2ODI4NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nTaS8WEcJa3jT4itR7JClWyx8rYzIe8fvhULosR19Lo",
+    },
+  };
+
+  //update Movies state with a random page of movies, from the top 50 pages of most popular movies at a given moment
+  const searchMovies = async function () {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc&page=1`,
+      options
+    );
+    const data = await response.json();
+    console.log(data);
+    setMovies(data.results);
+  };
+  console.log(movies);
+
+  useEffect(() => {
+    searchMovies();
+  }, []);
+
+  const handleClick = () => {};
+
   return (
     <div className="App">
-      <form className="Form">
-        <label htmlFor="genre" className="genre">
-          Genre:
-          <select id="genre">
-            <option value="" disabled>
-              Select a genre
-            </option>
-            <option value="action">Action</option>
-            <option value="comedy">Comedy</option>
-            <option value="horror">Horror</option>
-            <option value="thriller">Thriller</option>
-            <option value="sf">SF</option>
-            <option value="romance">Romance</option>
-            <option value="drama">Drama</option>
-          </select>
-        </label>
-        <label htmlFor="minIMDBScore" className="imdb">
-          Minimum IMDb Score:
-          <input
-            type="text"
-            id="minIMDBScore"
-            placeholder="Enter min IMDb score"
-          />
-        </label>
-        <button type="submit">Generate Movie</button>
-      </form>
+      <button className="generate" onClick={handleClick}>
+        Generate Movie
+      </button>
+      <MovieCard />
     </div>
   );
 }
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -7,9 +7,10 @@ export default function MovieCard() {
 
     return(
     <div className='movie'>
-        <img src="" alt="Movie" className='poster' />
+        <img src="https://a.cdn-hotels.com/gdcs/production80/d479/d1f8aa42-09b8-473e-a8d5-44f4cf690635.jpg?impolicy=fcrop&w=800&h=533&q=medium" alt="Movie" className='poster' />
         <h3 className='title'>Title</h3>
-        <h3 className='imdb-rating'>Imdb rating</h3>
+        <h3 className='rating'>Rating</h3>
+        <h4 className='release-date'>Release date</h4>
 
     </div>)
 }
